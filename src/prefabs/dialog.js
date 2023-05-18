8d4ee0c28c38c404bf9f2937e988d40d4021d99a
Alex Groff <amgroff@ucsc.edu>
2023-05-18 15:44:15 -0700

    Inital Dialog

---
+++ src/prefabs/dialog.js
@@ -0,0 +1,5 @@
+class Dialog extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame)
+    }
+}
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,7 +9,7 @@ class Menu extends Phaser.Scene {
 
     create() {
         this.testPhysicsObject = this.physics.add.body(game.config.width/2, game.config.height/2, 50,50);
-
+        
         this.time.delayedCall(2000, () => {
             this.testPhysicsObject.setCircle(25);
         }, null, this);
