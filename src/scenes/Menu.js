7ecc39cfdb33e88597142e976170fd5231fcbb4f
Marlene Inoue <mminoue@ucsc.edu>
2023-05-18 16:21:52 -0700

    added default circle asset, restarting menu scene

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,15 +4,19 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-
+//        this.load.path ='./assets/';
+//
+//        this.load.image('circle', './circle.png');        
     }
 
     create() {
-        this.testPhysicsObject = this.physics.add.body(game.config.width/2, game.config.height/2, 50,50);
+//        this.testPhysicsObject = this.physics.add.body(game.config.width/2, game.config.height/2, 50,50);
+//        this.point1 = this.add.image(game.config.width / 2, game.config.width / 2, 'circle');
+//        this.point2 = this.add.image(game.config.width / 4, game.config.width / 3, 'circle');
 
-        this.time.delayedCall(2000, () => {
-            this.testPhysicsObject.setCircle(25);
-        }, null, this);
+//        this.time.delayedCall(2000, () => {
+//            this.testPhysicsObject.setCircle(25);
+//        }, null, this);
     }
 
     update() {}
