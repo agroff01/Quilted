37777b14a59f8cadca944e9a61dd3b35c71c3c65
Alex Groff <amgroff@ucsc.edu>
2023-05-20 14:47:30 -0700

    Text pos changes with box pos

--- src/prefabs/dialog.js
+++ src/prefabs/dialog.js
@@ -1,43 +1,32 @@
 class Dialog extends Phaser.GameObjects.Sprite {
     constructor(scene, side, bodyText) {
 
-        let boxWidth = (side == 'center' ? game.config.width : game.config.width - 100), boxHeight = game.config.height/3;
-
-        const box = scene.textures.addDynamicTexture('box', boxWidth, boxHeight)
-
-        box.stamp('speechBubble', null, boxWidth/2, boxHeight/2)
-
-        // change this out for bitmapText!!!!!!!!!!!!
-        
-        const text = scene.make.text({
-            add: false,
-            text: bodyText,
-            style: {
-                fontSize: '64px',
-                fontFamily: 'CraftyGirls24',
-                color: '#ffffff',
-                align: 'center',
-                backgroundColor: '#ff00ff'
-            }
-        });
-
-        scene.textures.addCanvas('text', text.canvas);
-
-        box.stamp('text')
-
+        let x, y;
+        let textWidth = (side != 'center' ? 300 : 900)
 
         if (side == 'left') {
-            super(scene, boxWidth/2, game.config.height/2, 'box')
+            x = game.config.width /5
+            y = game.config.height * 4/5
 
         } else if (side == 'right') {
-            super(scene, game.config.width, game.config.height, 'box')
+            x = game.config.width
+            y = game.config.height
 
         } else if (side == 'center'){
-            super(scene, game.config.width * 3/4, game.config.height, 'box')
+            x = game.config.width * 3/4
+            y = game.config.height
 
-        } else 
-            console.log('Undifined Side on Dialog Box with :' + text)
-
+        } else {
+            console.log('Undifined Side on Dialog Box with :' + bodyText)
+        }
+        
+        super(scene, x, y, 'speechBubble').setOrigin(.5);
         scene.add.existing(this);
+
+        let btext = scene.add.bitmapText(x,y, "CraftyGirls24", bodyText).setOrigin(0.5).setCenterAlign().setMaxWidth(textWidth);
+        
+        this.x = x;
+        this.y = y;
+        this.textWidth = textWidth;
     }
 }
