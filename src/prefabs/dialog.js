512cec9ff10f9c4ef89baba99fee168f2f57b86d
Alex Groff <amgroff@ucsc.edu>
2023-05-21 17:59:07 -0700

    Text appears at a defined speed

--- src/prefabs/dialog.js
+++ src/prefabs/dialog.js
@@ -1,20 +1,22 @@
 class Dialog extends Phaser.GameObjects.Sprite {
-    constructor(scene, side, bodyText) {
+
+
+    constructor(scene, side, bodyText = '', isSlowText = true, textSpeed = 30) {
 
         let x, y;
         let textWidth = (side != 'center' ? 300 : 900)
 
         if (side == 'left') {
-            x = game.config.width /5
+            x = game.config.width / 5
             y = game.config.height * 4/5
 
         } else if (side == 'right') {
-            x = game.config.width
-            y = game.config.height
+            x = game.config.width * 3/4
+            y = game.config.height * 4/5
 
         } else if (side == 'center'){
-            x = game.config.width * 3/4
-            y = game.config.height
+            x = game.config.width / 2 
+            y = game.config.height * 4/5
 
         } else {
             console.log('Undifined Side on Dialog Box with :' + bodyText)
@@ -23,15 +25,29 @@ class Dialog extends Phaser.GameObjects.Sprite {
         super(scene, x, y, 'speechBubble').setOrigin(.5);
         scene.add.existing(this);
 
-        this.boxText = scene.add.bitmapText(x,y, "CraftyGirls24", bodyText).setOrigin(0.5).setCenterAlign().setMaxWidth(textWidth);
+        // if this is not a slow text box, display all text
+        this.boxText = scene.add.bitmapText(x,y, "CraftyGirls24", (isSlowText ? '' : bodyText)).setOrigin(0.5).setCenterAlign().setMaxWidth(textWidth);
         
+
         this.x = x;
         this.y = y;
         this.textWidth = textWidth;
+        this.scene = scene;
+        this.textSpeed = textSpeed;
     }
 
 
     changeText(newText) {
         this.boxText.setText(newText)
+    }
+
+    displaySlowText(fullText, textSpeeeeeed = this.textSpeed, textIndex = 0) {
+
+        if (textIndex >= fullText.length) return;
+        this.changeText(fullText.slice(0, textIndex))
+
+        this.scene.time.delayedCall(textSpeeeeeed, () => {
+            this.displaySlowText(fullText, textSpeeeeeed, textIndex+1)
+        }, null, this.scene);
     }
 }
